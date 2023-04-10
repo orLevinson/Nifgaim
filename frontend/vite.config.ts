e9d18b79cbfae087cb5c29a8048d329cff5f39ed
Or Levinson <or20022@gmail.com>
2023-04-10 20:39:57 +0300

    maybe this will work?

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["styled-components"],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["styled-components"],
+  //   },
+  // },
 });
